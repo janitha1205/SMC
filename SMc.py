8827b4076c3b545ad233b886a98425d38b32c1a9
janitha thalagoda <112193392+janitha1205@users.noreply.github.com>
2025-11-20 20:17:41 +0530

    SMc.py

--- SMc.py
+++ SMc.py
@@ -2,18 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Define the system dynamics (e.g., a simple mass-spring-damper)
-def system_dynamics(x, u, t):
-    m = 1.0  # mass
-    b = 0.5  # damping coefficient
-    k = 1.0  # spring constant
-    nk=0.08 #cross coupling
+class system:
 
-    # State variables: x[0] = position, x[1] = velocity
-    x_dot = np.zeros(2)
-    x_dot[0] = x[1]
-    x_dot[1] = (u - b * x[1] * x[1] - k * x[0] * x[0] - nk * x[0] * x[1]) / m
-    return x_dot
+    def __init__(self, m, b, k, nk):
+        self.m = m  # mass
+        self.b = b  # damping coefficient
+        self.k = k  # spring constant
+        self.nk = nk  # cross coupling
+        self.x = np.array([0, 0, 0])
+
+    def system_dynamics(self, x, xdot, u, dt):
+        xdotdot = (u - self.b * xdot - self.k * x - self.nk * x * xdot) / self.m
+        xdot = xdot + xdotdot * dt
+        x = x + xdot * dt + 0.5 * xdotdot * dt * dt
+        self.x = np.array([x, xdot, xdotdot])
+        return self.x
+
+    def det_eq_u(self, x, a):
+        u = (
+            a * self.m
+            + self.b * self.x[1]
+            + self.k * self.x[0]
+            + self.nk * self.x[0] * self.x[1]
+        )
+        return u
+
+
+
 
 
 # Define the desired trajectory (e.g., a step input to a target position)
@@ -21,62 +36,57 @@ def desired_trajectory(t):
     return 1.0, 0.0  # desired position, desired velocity
 
 
-# Sliding Mode Control implementation
-def sliding_mode_controller(x, x_d, x_d_dot, lambda_val, eta):
-    # Error definition
-    e = x[0] - x_d
-    e_dot = x[1] - x_d_dot
+class controls:
 
-    # Sliding surface definition (s = e_dot + lambda * e)
-    s = e_dot + lambda_val * e
-   
+    def __init__(self, lemda, eta):
+        self.lemda = lemda
+        self.eta = eta
 
-    # Equivalent control (for nominal system)
-    # Assuming u_eq = m * (x_d_ddot - lambda * e_dot) + b * x[1] + k * x[0]
-    # For simplicity here, we'll assume x_d_ddot is 0 for a constant desired position
-    # and focus on the switching part for robustness.
-    u_eq = 0  # In a full implementation, this would compensate for known dynamics
+    def find_u(self, x, xd):
+        self.e = x[0] - xd[0]
+        self.e_dot = x[1] - xd[1]
+        self.s = self.e_dot + self.lemda * self.e
+        self.usmd = -self.eta * self.s
+        return self.usmd, self.s, self.e, self.e_dot, self.lemda
 
-    # Switching control (to drive s to zero)
-    u_sw = -eta * np.sign(s)
 
-    # Total control input
-    u = u_eq + u_sw
-    return u,  s
 
 
 # Simulation parameters
 dt = 0.01  # time step
 t_end = 10.0  # simulation end time
 time = np.arange(0, t_end, dt)
-
+m = 1.0  # mass
+b = 0.5  # damping coefficient
+k = 1.0  # spring constant
+nk = 0.08  # cross coupling
 # Controller parameters
-lambda_val = 2.0  # slope of the sliding surface
-eta = 5.0  # switching gain (adjust for robustness vs. chattering)
+lambda_val = 2  # slope of the sliding surface
+eta = 5  # switching gain (adjust for robustness vs. chattering)
 
 # Initial conditions
-x = np.array([0.0, 0.0])  # initial position, initial velocity
+x = np.array([0.0, 0.0, 0.0])  # initial position, initial velocity
 
 # Store results
 positions = []
 velocities = []
 control_inputs = []
 sliding_surfaces = []
-e_dot_l = 0
+
+element = system(m, b, k, nk)
+consys = controls(lambda_val, eta)
 # Simulation loop
 for t in time:
     x_d, x_d_dot = desired_trajectory(t)
 
-    u, s = sliding_mode_controller(x, x_d, x_d_dot, lambda_val, eta)
-
-    # Apply control input and update system state
-    x_dot = system_dynamics(x, u, t)
-    x += x_dot * dt
+    usmd, s, e, e_dot, lemda = consys.find_u(x, np.array([x_d, x_d_dot]))
+    ueq = element.det_eq_u(x, -e_dot * lemda)
+    x = element.system_dynamics(x[0], x[1], ueq + usmd, dt)
 
     # Store data
     positions.append(x[0])
     velocities.append(x[1])
-    control_inputs.append(u)
+    control_inputs.append(ueq + usmd)
     sliding_surfaces.append(s)
 
 # Plotting results
